3cf4f20399364986150535c8c3d7fda6844d328c
sanjanabhogavalli <bhogavalli.sanjana@gmail.com>
2023-12-05 15:00:52 +0530

    recipe-app

---
+++ recipe-app/frontend/src/types.ts
@@ -0,0 +1,13 @@
+//export type Recipe = {}
+export interface Recipe{
+id: number,
+title: string,
+image: string,
+imageType: string
+}
+
+export interface RecipeSummary{
+    "id": number,
+  "title": string,
+  "summary": string
+}
